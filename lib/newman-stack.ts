346689e172ba33a454bf0f77f093107c50e66be1
sureshsahu <suresh.sahu@trangile.com>
2023-01-24 16:53:25 +0530

    commit

--- lib/newman-stack.ts
+++ lib/newman-stack.ts
@@ -1,16 +1,53 @@
 import * as cdk from 'aws-cdk-lib';
+import { SecretValue } from 'aws-cdk-lib';
+import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+import { CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NewmanStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const pipeline = new Pipeline(this, "newmanPipeline", {
+      pipelineName: 'newmanpipeline',
+      crossAccountKeys: false,
+      restartExecutionOnUpdate: true,
+    })
+    const cdkSourceOutput = new Artifact("CDKSourceOutput")
+    pipeline.addStage({
+      stageName: "Source",
+      actions:
+        [
+          new GitHubSourceAction({
+            owner: "Suresh14531453",
+            repo: "testRepo_aws",
+            branch: "master",
+            actionName: "Pipeline_Source",
+            oauthToken: SecretValue.secretsManager("token_access"),
+            output: cdkSourceOutput
+          }),
+        ],
+    })
+    const cdkBuildOutput = new Artifact("BuildOutput")
+    pipeline.addStage({
+      stageName: "Build",
+      actions: [
+        new CodeBuildAction({
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'NewmanQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+          actionName: "CDK_Build",
+          input: cdkSourceOutput,
+          outputs: [cdkBuildOutput],
+          project: new PipelineProject(this, "CdkBuildProject", {
+            environment: {
+              buildImage: LinuxBuildImage.STANDARD_5_0,
+            },
+            buildSpec: BuildSpec.fromSourceFilename(
+              "build-specs/cdk-build-spec.yml"
+            ),
+          }),
+        }),
+      ]
+    })
   }
 }
